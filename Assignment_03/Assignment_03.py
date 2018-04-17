a54762d9a406ebca0fc5889dc7f20f09f95234ef
anees <akulka2@stevens.edu>
2018-04-17 02:15:10 -0400

    First review

--- Assignment_03/Assignment_03.py
+++ Assignment_03/Assignment_03.py
@@ -3,10 +3,28 @@ from selenium.webdriver.support.select import Select
 import random
 import time
 import pandas as pd
-
-def extract_review(html, get_player_url=False):
-    soup = BeautifulSoup(data_html, 'lxml')
-
+import requests
+import bs4
+from bs4 import BeautifulSoup
+import random
+import time
 
 driver = webdriver.Firefox(executable_path=r'C:\Users\anees\geckodriver.exe')
-driver.get('https://www.amazon.com/RockBirds-Flashlights-Bright-Aluminum-Flashlight/product-reviews/B00X61AJYM')
+html=driver.get('https://www.amazon.com/RockBirds-Flashlights-Bright-Aluminum-Flashlight/product-reviews/B00X61AJYM')
+
+
+start_time = time.time()
+normal_delay = random.normalvariate(2, 0.5)
+time.sleep(normal_delay)
+print("--- %.5f seconds ---" % (time.time() - start_time))
+
+def delay(t):
+    normal_delay = random.normalvariate(t, 0.5)
+    time.sleep(normal_delay)
+
+    delay(3)
+html="https://www.amazon.com/RockBirds-Flashlights-Bright-Aluminum-Flashlight/product-reviews/B00X61AJYM"
+soup = bs4.BeautifulSoup(requests.get(html).text, 'lxml')
+big_box = soup.find('div', attrs={'class': 'a-expander-content a-expander-partial-collapse-content'})
+print (big_box.prettify())
+
